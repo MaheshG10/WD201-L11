2f60f12bfbb7bae6da23903a7d9017e302a0dee2
MaheshG10 <92197783+MaheshG10@users.noreply.github.com>
2023-01-09 22:26:54 +0530

    L11 updted

--- migrations/20221208201558-voter_creation.js
+++ migrations/20221208201558-voter_creation.js
@@ -1,34 +1,34 @@
-"use strict";
+'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Voters", {
+    await queryInterface.createTable('Voters', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER
       },
       voterID: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING
       },
       password: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING
       },
       voted: {
-        type: Sequelize.BOOLEAN,
+        type: Sequelize.BOOLEAN
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE,
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-      },
+        type: Sequelize.DATE
+      }
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Voters");
-  },
+    await queryInterface.dropTable('Voters');
+  }
 };
